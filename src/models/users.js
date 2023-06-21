2fb050a2cd0dee0f1cbfcfa2e1ec470c11b27690
Sidddanth Katipally <siddanth6365@gmail.com>
2023-06-21 18:58:44 +0530

    Added encryption for signup passwords

--- src/models/users.js
+++ src/models/users.js
@@ -1,39 +1,43 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
+
+const bcrypt = require("bcrypt");
+
 module.exports = (sequelize, DataTypes) => {
   class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // define association here
     }
   }
-  Users.init({
-    
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true
-      }
+  Users.init(
+    {
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [3, 50],
+        },
+      },
     },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [3, 50]
-      }
-    },
-    
-  }, {
-    sequelize,
-    modelName: 'Users',
+    {
+      sequelize,
+      modelName: "Users",
+    }
+  );
+
+  Users.beforeCreate(async(user) => {
+    const SALT_ROUNDS=8;
+    const encryptedPassword = bcrypt.hashSync(user.password, SALT_ROUNDS);
+    user.password = encryptedPassword;
   });
+
   return Users;
 };
