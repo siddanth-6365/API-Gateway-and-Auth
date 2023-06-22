bc50498d799a68d53435e01a9561ff673e1e9651
Sidddanth Katipally <siddanth6365@gmail.com>
2023-06-22 14:20:24 +0530

    added middlewares for auth

--- src/routes/v1/UserRoutes.js
+++ src/routes/v1/UserRoutes.js
@@ -1,16 +1,23 @@
-const express = require('express');
-const { InfoController,UserController } = require('../../controllers');
-
+const express = require("express");
+const { InfoController, UserController } = require("../../controllers");
+const { usermiddleware } = require("../../middlewares");
 
 const router = express.Router();
 
 /**
- * POST : /signup 
+ * POST : /signup
  * req-body {email: 'a@b.com', password: '1234'}
  */
 
-router.post('/signup', UserController.signup);
-router.post('/signin', UserController.signin);
-
+router.post(
+  "/signup",
+  usermiddleware.validateAuthRequest,
+  UserController.signup
+);
+router.post(
+  "/signin",
+  usermiddleware.validateAuthRequest,
+  UserController.signin
+);
 
 module.exports = router;
