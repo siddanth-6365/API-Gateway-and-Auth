555086eac5261494200b96515ece06e651e64c50
Sidddanth Katipally <siddanth6365@gmail.com>
2023-06-23 13:00:40 +0530

    added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
-
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 const app = express()
 
-var bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit')
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
+var bodyParser = require('body-parser');
 // configure the app to use bodyParser()
 app.use(bodyParser.urlencoded({
-    extended: true
-}));
+    extended: true}));
 app.use(bodyParser.json());
+
+//rate limiter
+const limiter = rateLimit({
+	windowMs: 15* 60 * 1000, // 15 minutes
+	max: 100 // Limit max of 100 requests in every 15 mins
+})
+app.use(limiter) // before  url/api we have to apply the limiter
+
+// proxy
+app.use('/flightService', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true, }))
+app.use('/BookingService', createProxyMiddleware({ target: 'http://localhost:5000', changeOrigin: true ,
+pathRewrite: {
+    '/BookingService': '/', // rewrite path means no need to mention in boooking service routes
+ }}))
 
 
 app.use('/api', apiRoutes);
